0eba977476ea6c9d277c5b0a0990dbb7a1ee44a7
devld <devlduio@gmail.com>
2018-02-07 19:52:13 +0800

    add class header

--- coursera/Algorithms-Part-I/interview-questions/week-01/src/EggDrop.java
+++ coursera/Algorithms-Part-I/interview-questions/week-01/src/EggDrop.java
@@ -1,3 +1,19 @@
+/**
+ * <h3>Egg drop</h3>
+ *
+ * Suppose that you have an n-story building (with floors 1 through n) and plenty of eggs.
+ * An egg breaks if it is dropped from floor T
+ * or higher and does not break otherwise. 
+ * Your goal is to devise a strategy to determine the value of T given
+ * the following limitations on the number of eggs and tosses:
+ * 
+ *  - Version 0: 1 egg, ≤T tosses.
+ *  - Version 1: ∼1lgn eggs and ∼1lgn tosses.
+ *  - Version 2: ∼lgT eggs and ∼2lgT tosses.
+ *  - Version 3: 2 eggs and ∼2n√ tosses.
+ *  - Version 4: 2 eggs and ≤cT−−√ tosses for some fixed constant c.
+*/
+
 public class EggDrop {
 
     public static void v0(int topFloor, Toss toss) {
